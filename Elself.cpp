8f51fee3fc2e42cf4f5658b9a4988cf438241456
crisjame09-stack <crisjame09@gmail.com>
2026-03-31 11:01:39 +0630

    Add files via upload

---
+++ Elself.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+float Average(float a, float b){
+    return (a+b)/2;
+}
+
+string GraduationStatus(float r){
+    if (r >= 60)
+        return "Passed";
+    else
+        return "Failed";
+}
+
+int main(){
+    float engScore, mathScore, average;
+    string status;
+
+    cout << "Enter English score = ";
+    cin >> engScore;
+    cout << "Enter Math score = ";
+    cin >> mathScore;
+
+    average = Average(engScore, mathScore);
+
+    cout << "Graduation status = " << GraduationStatus(average) << " ,with average score = " << average << endl;
+}
---
+++ NestedIf.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+float Average(float a, float b){
+    return (a+b)/2;
+}
+
+string GraduationStatus(float r){
+    if (r >= 60)
+        if(r > 80){
+            return "Graduated with Distinction";
+        }
+        else{
+            return "Graduated with Good Standing";
+        }
+    else
+        return "Failed";
+}
+
+int main(){
+    float engScore, mathScore, average;
+    string status;
+
+    cout << "Enter English score = ";
+    cin >> engScore;
+    cout << "Enter Math score = ";
+    cin >> mathScore;
+
+    average = Average(engScore, mathScore);
+
+    cout << "Graduation status = " << GraduationStatus(average) << " ,with average score = " << average << endl;
+}
